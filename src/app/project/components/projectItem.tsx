263f9a72867ba8eb1a1aeae3754be5eddb4f681c
joaovq <joaovqdev@gmail.com>
2023-12-01 16:20:34 -0300

    fix: fix projects space

--- src/app/project/components/projectItem.tsx
+++ src/app/project/components/projectItem.tsx
@@ -6,7 +6,7 @@ const Container = styled.div<{ $colorPrimary: string, $colorSecondary:string,  }
 display: flex;
 flex-direction: column;
 gap: 2rem;
-max-width: 200px;
+max-width: 300px;
 width: 100%;
 height: 100%;
 .content {
