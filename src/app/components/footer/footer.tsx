36c7ad187e0d279b0feefd50be5556ffa0b644bd
joaovq <joaovqdev@gmail.com>
2023-12-04 15:42:47 -0300

    fix footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container =  styled.footer`
-    @use '../../page.module.scss' as v;
     background: #1E1E1E;
     display: flex;
     justify-content: center;
